53b5e3ccb778efe7215ba4f768a6d9d5f0aca965
Alex Bokov <bokov@uthscsa.edu>
2020-01-10 15:52:46 -0600

    ci_parent_test.R; ci; Oops. Was using the non-raw URL for 'quickstart.R'. No wonder

--- ci_parent_test.R
+++ ci_parent_test.R
@@ -1,5 +1,5 @@
 # config
-ripcord <- 'https://github.com/bokov/ripcord/blob/%s/quickstart.R';
+ripcord <- 'https://raw.githubusercontent.com/bokov/ripcord/%s/quickstart.R';
 test_branch <- 'enh_wincompat';
 ripcord <- sprintf(ripcord,test_branch);
 
@@ -8,7 +8,7 @@ dir.create('test_localdata');
 file.copy('localdata','test_localdata/',recursive = TRUE);
 file.copy(list.files(patt='\\.auto\\.menu.*\\.R',all=TRUE),'test_localdata/');
 file.copy('localdataonefile.auto.confch.R','test_localdata/');
-setwd('test_localdata/');
+setwd('test_localdata');
 source(ripcord);
 source('ci_test.R');
 setwd('..');
