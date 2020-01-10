c6df73aac151f71da27360b46c860336d5abefda
Alex Bokov <bokov@uthscsa.edu>
2020-01-10 14:20:41 -0600

    .travis.yml,ci_parent_test.R; ci; Added files for CI of parent project

---
+++ ci_parent_test.R
@@ -0,0 +1,15 @@
+# config
+ripcord <- 'https://github.com/bokov/ripcord/blob/%s/quickstart.R';
+test_branch <- 'enh_wincompat';
+ripcord <- sprintf(ripcord,test_branch);
+
+# test localdata
+dir.create('test_localdata');
+file.copy('localdata','test_localdata/',recursive = TRUE);
+file.copy(list.files(patt='\\.auto\\.menu.*\\.R',all=TRUE),'test_localdata/');
+file.copy('localdataonefile.auto.confch.R','test_localdata/');
+setwd('test_localdata/');
+source('ripcord');
+setwd('..');
+c()
+
